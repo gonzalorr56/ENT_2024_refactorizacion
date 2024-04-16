d0cdeaec74156b4912204c6ff0333160d1f91a62
Mañana <Maana@DESKTOP-Q3J159D>
2024-04-16 10:11:40 +0200

    v3

--- src/ejercicio2/Pedido.java
+++ src/ejercicio2/Pedido.java
@@ -5,10 +5,6 @@ import java.util.List;
 public class Pedido {
 	  private List<Pedido> pedidos;
 
-	    public void realizarPedido(Cliente cliente, List<Producto> productos) {
-	        // Lógica para realizar un pedido
-	    }
-
 	    public double calcularPrecioTotalPedido(Pedido pedido) {
 	        // Lógica para calcular el precio total de un pedido
 	    }
--- src/ejercicio2/Tienda.java
+++ src/ejercicio2/Tienda.java
@@ -1,17 +1,29 @@
 package ejercicio2;
 
-import java.util.List;
-
 public class Tienda {
     private List<Producto> productos;
     private List<Cliente> clientes;
     private List<Pedido> pedidos;
 
-    public static void main(String[] args) {
-		Tienda tienda = new Tienda();
-	    Productos producto = new Productos();
-	    Cliente cliente = new Cliente();
-	    Pedido pedido = new Pedido();
-}
+    public void agregarProducto(Producto producto) {
+        // Lógica para agregar un producto
+    }
+
+    public void eliminarProducto(Producto producto) {
+        // Lógica para eliminar un producto
+    }
+
+    public void agregarCliente(Cliente cliente) {
+        // Lógica para agregar un cliente
+    }
+
+    public void eliminarCliente(Cliente cliente) {
+        // Lógica para eliminar un cliente
+    }
+
+    public void realizarPedido(Cliente cliente, List<Producto> productos) {
+        // Lógica para realizar un pedido
+    }
+
     // Otros métodos relacionados con la gestión de la tienda
 }
