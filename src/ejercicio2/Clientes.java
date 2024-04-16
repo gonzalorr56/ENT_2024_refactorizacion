6b7fd1ccb904530cbce94be9e8bd3a4dc616bb24
Mañana <Maana@DESKTOP-Q3J159D>
2024-04-16 10:01:02 +0200

    v1 tienda

---
+++ src/ejercicio2/Clientes.java
@@ -0,0 +1,15 @@
+package ejercicio2;
+
+import java.util.List;
+
+public class Clientes {
+    private List<Cliente> clientes;
+
+    public void agregarCliente(Cliente cliente) {
+        // Lógica para agregar un cliente
+    }
+
+    public void eliminarCliente(Cliente cliente) {
+        // Lógica para eliminar un cliente
+    }
+}
---
+++ src/ejercicio2/Start.java
@@ -0,0 +1,8 @@
+package ejercicio2;
+
+public class Start {
+	Tienda tienda = new Tienda();
+    Productos productos = new Productos();
+    Clientes clientes = new Clientes();
+    Pedidos pedidos = new Pedidos();
+}
