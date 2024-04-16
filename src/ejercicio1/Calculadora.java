c759e715bf6d5a6a31ff7abd043e57e163e4b457
Mañana <Maana@DESKTOP-Q3J159D>
2024-04-16 09:33:46 +0200

    v2

--- src/ejercicio1/Calculadora.java
+++ src/ejercicio1/Calculadora.java
@@ -1,23 +1,23 @@
 package ejercicio1;
 
 public class Calculadora {
-    public int sumar(int a, int b) {
-        return a+b;
+    public int sumar(int uno, int dos) {
+        return uno+dos;
     }
 
-    public int restar(int a, int b) {
-        return a-b;
+    public int restar(int uno, int dos) {
+        return uno-dos;
     }
 
-    public int multiplicar(int a, int b) {
-        return a * b;
+    public int multiplicar(int uno, int dos) {
+        return uno * dos;
     }
 
-    public int dividir(int a, int b) {
-        if (b == 0) {
+    public int dividir(int uno, int dos) {
+        if (dos == 0) {
             throw new ArithmeticException("No se puede dividir entre 0");
         } else {
-            return a / b;
+            return uno / dos;
         }
     }
 }
